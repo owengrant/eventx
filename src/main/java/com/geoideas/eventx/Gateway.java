fa027273fde520d147fd13845873e18044695766
Owen Grant <owen.dalmon.grant@gmail.com>
2019-03-12 23:03:26 -0400

    added socjks eventbus bridge

--- src/main/java/com/geoideas/eventx/Gateway.java
+++ src/main/java/com/geoideas/eventx/Gateway.java
@@ -4,6 +4,10 @@ package com.geoideas.eventx;
 import com.geoideas.eventx.service.consumer.EventService;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.DeploymentOptions;
+import io.vertx.ext.bridge.PermittedOptions;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.sockjs.BridgeOptions;
+import io.vertx.ext.web.handler.sockjs.SockJSHandler;
 import io.vertx.serviceproxy.ServiceBinder;
 
 
@@ -12,6 +16,7 @@ public final class Gateway extends AbstractVerticle {
     @Override
     public void start() {
         setup();
+        bridgeEventbus();
         System.out.println("SPOOCH EVENT STORE SERVICE RUNNING");
     }
 
@@ -22,5 +27,22 @@ public final class Gateway extends AbstractVerticle {
         vertx.deployVerticle(publisher, new DeploymentOptions().setConfig(config()));
         var consumer = "com.geoideas.eventx.service.consumer.ConsumerVerticle";
         vertx.deployVerticle(consumer, new DeploymentOptions().setConfig(config()));
+    }
+    
+    public void bridgeEventbus(){
+        var bridge = SockJSHandler.create(vertx);
+        var inbound = new PermittedOptions().setAddressRegex(config().getString("address"));
+        var ops = new BridgeOptions().addInboundPermitted(inbound);
+        bridge.bridge(ops);
+        var router = Router.router(vertx);
+        router.route("/eventbus/*").handler(bridge);
+        var serverConfig = config().getJsonObject("bridge");
+        vertx.createHttpServer()
+             .requestHandler(router)
+             .listen(
+                     serverConfig.getInteger("port"),
+                     serverConfig.getString("host"),
+                     hndlr -> System.out.println(hndlr.succeeded() ? "EVENTBUS BRIDGE INTEGRATION RUNNING" : "FAIL TO OPEN EVENTBUS BRIDGE")
+             );
     }
 }
